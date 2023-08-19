8549a1d6ce51178aa768db1964f6fe4d50ce4d60
Muhammad Usman <tmusman5@gmail.com>
2023-08-19 22:55:36 +0500

    add: async await example

---
+++ async-await.js
@@ -0,0 +1,29 @@
+const userLogin = () => {
+    console.log("Enter user_name and password")
+    let user_name = prompt("Enter username: ")
+    let password = prompt("Enter password: ")
+
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("Performing User Authentication");
+            if (user_name == 'Usman' && password == '123'){
+                resolve("User Authenticated")
+            }else{
+                reject("Authentication Failed!")
+            }
+        }, 1000)
+    })
+}
+
+function goToHomePage(userAuthStatus){
+    return Promise.resolve("Go To Homepage as "+userAuthStatus)
+}
+
+
+userLogin().then(valid=>{
+    console.log("Validated User");
+    return goToHomePage(valid)
+}
+).then(userAuthStatus => console.log(userAuthStatus)
+).catch(err => console.log(err)
+).finally(()=>console.log("Finishings Up!"))
