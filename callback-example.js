4a554a2b3bb6ce53e0af4c013dfd6583dbd624b7
Muhammad Usman <tmusman5@gmail.com>
2023-08-19 14:59:36 +0500

    add: call back example

---
+++ callback-example.js
@@ -0,0 +1,45 @@
+// console.log("Task Start")
+
+// function asyncTask (cb){
+//     console.log("Task Runnning")
+//     // cb();
+//     setTimeout(cb,0)
+// }
+
+
+// asyncTask(()=>console.log(name));
+// console.log("Task End")
+// const name = "Usman"
+
+
+
+function asyncTask(cb) {
+    // setTimeout(() => {
+    //      cb()
+
+    // }, 0
+    // )
+    cb(null,1);
+}
+
+
+// asyncTask((err, data) => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log('data', data)
+// })
+
+function makeApiCall(cb){
+    setTimeout(()=>{
+        console.log("This is async task exec")
+    }, 0)
+}
+
+makeApiCall(()=>{
+    makeApiCall(()=>{
+        makeApiCall(()=>{
+    // call back hell
+        })  
+    })  
+})
