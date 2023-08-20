66ade3b14899d2e4dbc76016f20eb6e1cb77ef46
Muhammad Usman <tmusman5@gmail.com>
2023-08-20 23:36:14 +0500

    add: server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const http  = require("http");
+const {config} = require("dotenv");
+config();
+
+
+const PORT = process.env.PORT || 5001
+
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify({message:"Hello World"}))
+    res.end()
+});
+
+
+server.listen(PORT,()=>{
+    console.log("Server started on post: "+PORT)
+})
+
