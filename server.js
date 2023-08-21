26872e9efb30e9a1afdf29bf6bb3e907aba5966d
Muhammad Usman <tmusman5@gmail.com>
2023-08-21 20:51:18 +0500

    changes: http example

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-const http  = require("http");
-const {config} = require("dotenv");
+const http = require('http');
+const { config } = require('dotenv');
+
 config();
 
+const PORT = process.env.PORT || 5001;
 
-const PORT = process.env.PORT || 5001
-
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({message:"Hello World"}))
-    res.end()
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/json');
+	res.write(JSON.stringify({ message: 'Hello World' }));
+	res.end();
 });
 
-
-server.listen(PORT,()=>{
-    console.log("Server started on post: "+PORT)
-})
-
+server.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
