a23469785cc2644f2aaeb857c176a174e7e19679
Muhammad Usman <tmusman5@gmail.com>
2023-08-21 20:51:43 +0500

    add: index.js for express entry point

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { config } = require("dotenv");
+
+config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.get("/users", (req, res) => {
+  res.json({ message: "get all users " });
+});
+
+app.get("/users/:id", (req, res) => {
+  res.json({ message: `Get user with id ${req.params.id}` });
+});
+
+app.post("/users", (req, res) => {
+  res.json({ message: "Create a new user" });
+});
+
+app.put("/users/:id", (req, res) => {
+  res.json({ message: `Update user with id ${req.params.id}` });
+});
+
+app.delete("/users/:id", (req, res) => {
+  res.json({ message: `Delete user with id ${req.params.id}` });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example App listining on port ${PORT}`);
+});
