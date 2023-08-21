047d3de10348ccab6c114aca1a6a7f8a83093d79
Muhammad Usman <tmusman5@gmail.com>
2023-08-21 21:07:41 +0500

    changes: implementing application level middleware

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const { config } = require("dotenv");
+const express = require('express');
+const { config } = require('dotenv');
 
 config();
 
@@ -7,30 +7,37 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+const loggerMiddleware = (req, res, next) => {
+	console.log(`${new Date()} --- request [${req.method}] [${req.url}]`);
+	next();
+};
+
+app.use(loggerMiddleware);
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Hello World' });
 });
 
-app.get("/users", (req, res) => {
-  res.json({ message: "get all users " });
+app.get('/users', (req, res) => {
+	res.json({ message: 'get all users ' });
 });
 
-app.get("/users/:id", (req, res) => {
-  res.json({ message: `Get user with id ${req.params.id}` });
+app.get('/users/:id', (req, res) => {
+	res.json({ message: `Get user with id ${req.params.id}` });
 });
 
-app.post("/users", (req, res) => {
-  res.json({ message: "Create a new user" });
+app.post('/users', (req, res) => {
+	res.json({ message: 'Create a new user' });
 });
 
-app.put("/users/:id", (req, res) => {
-  res.json({ message: `Update user with id ${req.params.id}` });
+app.put('/users/:id', (req, res) => {
+	res.json({ message: `Update user with id ${req.params.id}` });
 });
 
-app.delete("/users/:id", (req, res) => {
-  res.json({ message: `Delete user with id ${req.params.id}` });
+app.delete('/users/:id', (req, res) => {
+	res.json({ message: `Delete user with id ${req.params.id}` });
 });
 
 app.listen(PORT, () => {
-  console.log(`Example App listining on port ${PORT}`);
+	console.log(`Example App listining on port ${PORT}`);
 });
