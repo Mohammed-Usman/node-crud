1812fab9ced85feab87b06d232d735900ee998fd
Muhammad Usman <tmusman5@gmail.com>
2023-08-23 16:28:34 +0500

    add: middleware/errorHandler.js

---
+++ middleware/errorHandler.js
@@ -0,0 +1,51 @@
+const { constants } = require('../constants');
+
+module.exports = (err, req, res, next) => {
+	const statusCode = res.statusCode ? res.statusCode : 500;
+	res.status(statusCode);
+
+	switch (statusCode) {
+		case constants.VALIDATION:
+			res.json({
+				title: 'Validation Failed',
+				message: err.message,
+				stackTrace: err.stackTrace
+			});
+			break;
+		case constants.UNAUTHORIZED:
+			res.json({
+				title: 'Unathorized',
+				message: err.message,
+				stackTrace: err.stackTrace
+			});
+			break;
+
+		case constants.NOT_FOUND:
+			res.json({
+				title: 'Not Found',
+				message: err.message,
+				stackTrace: err.stackTrace
+			});
+			break;
+
+		case constants.FORBIDDEN:
+			res.json({
+				title: 'Forbidden',
+				message: err.message,
+				stackTrace: err.stackTrace
+			});
+			break;
+		case constants.SERVER_ERROR:
+			res.json({
+				title: 'Server Error',
+				message: err.message,
+				stackTrace: err.stackTrace
+			});
+			break;
+		default:
+			console.log('No Error');
+			break;
+	}
+};
+
+// module.exports = (err, req, res, next) => {};
