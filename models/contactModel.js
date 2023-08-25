30017447a6262d9327c877232ccb7abefb8cc02a
Muhammad Usman <tmusman5@gmail.com>
2023-08-25 20:59:59 +0500

    changes: contact model now contain user_id upon contact creation

--- models/contactModel.js
+++ models/contactModel.js
@@ -2,17 +2,22 @@ const mongoose = require('mongoose');
 
 const contactSchema = mongoose.Schema(
 	{
+		user_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: [true],
+			ref: 'User'
+		},
 		name: {
 			type: String,
-			require: [true, 'please add contact name']
+			required: [true, 'please add contact name']
 		},
 		email: {
 			type: String,
-			require: [true, 'please add contact email']
+			required: [true, 'please add contact email']
 		},
 		phone: {
 			type: String,
-			require: [true, 'please add contact phone']
+			required: [true, 'please add contact phone']
 		}
 	},
 	{
